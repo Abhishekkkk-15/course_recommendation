e132ae223f2c280755e2c79de893c213aef54004
Abhishek Jangid <mrabhi748@gmail.com>
2026-09-08 18:26:42 +0530

    test(retrival_evals): added MRR score

--- evals/retrival_evals.ts
+++ evals/retrival_evals.ts
@@ -8,6 +8,8 @@ const csvPath = path.join(process.cwd(), "static", "Course_Dummy_Data.csv");
 
 type TRetrivalMetrics = {
   recall: number;
+  // Mean Reciprocal Rank (MRR)
+  mrr: number;
 };
 
 const llm = new LLM();
@@ -17,6 +19,7 @@ const dataset = await loadDataSet(csvPath);
 
 export const metrics: TRetrivalMetrics = {
   recall: 0,
+  mrr: 0,
 };
 
 const limitedData = limitDataSet(evaleSuite, 16);
@@ -25,7 +28,7 @@ export async function run() {
   await store.addData(dataset);
 
   let totalRecallScore = 0;
-
+  let totalMRRScore = 0;
   for (const element of limitedData) {
     try {
       await wait(1000);
@@ -37,15 +40,31 @@ export async function run() {
       );
 
       const retrievedIds: string[] = res.map((e) => String(e.metadata.id));
-      const expectedIdStr = String(element.expectedId);
+      const expectedIdStr =
+        element.expectedId !== null ? String(element.expectedId) : "None";
 
-      const found = retrievedIds.includes(expectedIdStr);
+      let found = false;
+      let reciporalRank = 0;
+
+      if (element.expectedId !== null) {
+        const foundRank = retrievedIds.indexOf(String(element.expectedId));
+        if (foundRank !== -1) {
+          found = true;
+          reciporalRank = 1 / (foundRank + 1);
+        }
+      } else {
+        if (retrievedIds.length === 0 || element.expectedShowAll) {
+          found = true;
+          reciporalRank = 1.0;
+        }
+      }
+
       const queryRecall = found ? 1 : 0;
 
       totalRecallScore += queryRecall;
-
+      totalMRRScore += reciporalRank;
       console.log(
-        `Query: "${element.userQuery}" | Expected: ${expectedIdStr} | Found: ${found} | MRR ${"True"}`,
+        `Query: "${element.userQuery}" | Expected: ${expectedIdStr} | Found: ${found} | Reciprocal Rank: ${reciporalRank.toFixed(2)}`,
       );
     } catch (error) {
       console.error("Error processing query:", error);
@@ -54,8 +73,9 @@ export async function run() {
 
   metrics.recall =
     limitedData.length > 0 ? totalRecallScore / limitedData.length : 0;
-
+  metrics.mrr = limitedData.length > 0 ? totalMRRScore / limitedData.length : 0;
   console.log("Final Evaluated Metrics:", {
     recall: `${(metrics.recall * 100).toFixed(2)}%`,
+    mrr: `${(metrics.mrr * 100).toFixed(2)}% (Score: ${metrics.mrr.toFixed(3)})`,
   });
 }
